0b64685f50be4445157faf82fa514ae2e6b4d379
TheSearchGeeks <125513043+TheSearchGeeks@users.noreply.github.com>
2024-04-25 20:19:49 -0400

    dds
    dds

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/nba/stats/:gameId', async (req, res) => {
  * Schedule game checks to start at 7 PM EST every day.
  * Note: '0 16 * * *' runs at 16:00 UTC, which is 12:00 PM EST. Adjust according to daylight saving time.
  */
-schedule.scheduleJob('00 22 * * *', function() {
+schedule.scheduleJob('21 00 * * *', function() {
     console.log(`${new Date().toISOString()} - Setting up game checks...`);
     setupGameChecks();
 });
